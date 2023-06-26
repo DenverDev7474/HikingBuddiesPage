64ded4f3d81dcb757e66178ee3184cde2659da52
Hugo Amorim <hugo.amorim.dev@gmail.com>
2023-06-26 13:14:23 -0600

    background

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,19 @@
-/** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      container: {
+        center: true,
+        padding: 0,
+        margim: 0,
+      },
+      height: {
+        '1/2screen': '50vh',
+      }
+    },
   },
   plugins: [],
-}
-
+};
